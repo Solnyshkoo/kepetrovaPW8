43677b1931e5a0f25285453f5fe59ef0f1f1052c
Solnyshkoo <kepetrova@edu.hse.ru>
2022-03-17 18:28:17 +0300

    Initial Commit

---
+++ kepetrovaPW8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kepetrovaPW8
+//
+//  Created by Ksenia Petrova on 17.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
