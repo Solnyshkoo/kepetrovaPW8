611a54b1c7d2c1f7951f757860ff472209991b1e
Solnyshkoo <kepetrova@edu.hse.ru>
2022-03-17 18:56:54 +0300

    1 point

---
+++ kepetrovaPW8/View/MovieCel.swift
@@ -0,0 +1,52 @@
+//
+//  MovieCel.swift
+//  kepetrovaPW8
+//
+//  Created by Ksenia Petrova on 17.03.2022.
+//
+
+import Foundation
+import UIKit
+final class MovieCel: UITableViewCell {
+    static let indentifier = "MovieCell"
+
+    private lazy var title: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var poster: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    init() {
+        super.init(style: .default, reuseIdentifier: Self.indentifier)
+        configureUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        addSubview(poster)
+        addSubview(title)
+        NSLayoutConstraint.activate([
+            poster.topAnchor.constraint(equalTo: topAnchor),
+            poster.leadingAnchor.constraint(equalTo: leadingAnchor),
+            poster.trailingAnchor.constraint(equalTo: trailingAnchor),
+            poster.heightAnchor.constraint(equalToConstant: 200),
+            
+            title.topAnchor.constraint(equalTo: poster.bottomAnchor, constant: 10),
+            title.leadingAnchor.constraint(equalTo: leadingAnchor),
+            title.trailingAnchor.constraint(equalTo: trailingAnchor),
+            title.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
--- kepetrovaPW8/View/MoviesViewController.swift
+++ kepetrovaPW8/View/MoviesViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MoviesViewController: UIViewController {
+    private var tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
 
-
+    private func configureUI() {}
 }
-
